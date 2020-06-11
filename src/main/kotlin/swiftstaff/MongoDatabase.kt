9634cacbd41d5f3c6698e0afd52ba0e9026a7e64
TerenceHernandez <t.na.m.hernandez@gmail.com>
2020-06-11 13:35:42 +0100

    worker_profile_backend merging artefacts

--- src/main/kotlin/swiftstaff/MongoDatabase.kt
+++ src/main/kotlin/swiftstaff/MongoDatabase.kt
@@ -29,12 +29,12 @@ object MongoDatabase :  Database {
     val imageBucket: GridFSBucket
 
     init {
-//        val username: String = System.getenv("DB_USERNAME") ?: "mongoadmin"
-//        val password: String = System.getenv("DB_PASSWORD") ?: "mongoadmin"
-//        val server: String = System.getenv("SERVER_ADDRESS") ?: "127.0.0.1"
-        val username: String = System.getenv("DB_USERNAME") ?: "superuser"
-        val password: String = System.getenv("DB_PASSWORD") ?: "changeMeToAStrongPassword"
-        val server: String = System.getenv("SERVER_ADDRESS") ?: "localhost:27017"
+        val username: String = System.getenv("DB_USERNAME") ?: "mongoadmin"
+        val password: String = System.getenv("DB_PASSWORD") ?: "mongoadmin"
+        val server: String = System.getenv("SERVER_ADDRESS") ?: "127.0.0.1"
+//        val username: String = System.getenv("DB_USERNAME") ?: "superuser"
+//        val password: String = System.getenv("DB_PASSWORD") ?: "changeMeToAStrongPassword"
+//        val server: String = System.getenv("SERVER_ADDRESS") ?: "localhost:27017"
         val databaseName: String = System.getenv("DATABASE") ?: "test"
         this.connectionString = ConnectionString("mongodb://$username:$password@$server")
         this.mongoClient = KMongo.createClient(connectionString)
